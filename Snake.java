e1d5ba2461d9c443da2570aec8dcd0cea1bc4aa7
numlord17 <numlord17@gmail.com>
2026-03-06 09:39:59 -0800

    example

--- Snake.java
+++ Snake.java
@@ -10,34 +10,23 @@ import javax.imageio.ImageIO;
 import java.awt.event.KeyEvent;
 import java.util.Random;
 
-public class Snake {
-    public static void main(String[] args) {
-        try {
-            int i = 0;
-            while (true)
-            {
-            i++;
+
+         i++;
             Robot robot = new Robot();
             Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-            Rectangle screenRect = new Rectangle(screenSize);
             BufferedImage capture = robot.createScreenCapture(screenRect);
             File outputFile = new File("./screenshot" + i + ".png");
             String format = "png";
+
+
             ImageIO.write(capture, format, outputFile);
 
             System.out.println("Screenshot saved to: " + outputFile.getAbsolutePath());
-            
-            Robot snake = new Robot();
-            Random random = new Random();
-            int targetX = 500;
-            int targetY = 300;
-            while (true)
-            {
-            targetX = random.nextInt(1000);
-            targetY = random.nextInt(1000);
-            snake.keyPress(KeyEvent.VK_ESCAPE);
-            snake.keyRelease(KeyEvent.VK_ESCAPE);
-            snake.mouseMove(targetX, targetY);
+
+
+            ImageIO.write(capture, format, outputFile);
+
+            System.out.println("Screenshot saved to: " + outputFile.getAbsolutePath());
             }
 
         } catch (AWTException | IOException ex) {
